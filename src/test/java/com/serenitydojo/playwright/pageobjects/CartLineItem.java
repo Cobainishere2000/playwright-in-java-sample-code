b9a9b1e1e859b2b8db8cab33c4053ea95a8241fa
Cobainishere2000 <sapchester85@gmail.com>
2026-04-30 01:24:22 +0530

    implemented pageobjects package

---
+++ src/test/java/com/serenitydojo/playwright/pageobjects/CartLineItem.java
@@ -0,0 +1,5 @@
+package com.serenitydojo.playwright.pageobjects;
+
+public record CartLineItem (String title, int quantity, double price, double total){
+}
+
---
+++ src/test/java/com/serenitydojo/playwright/pageobjects/NavBar.java
@@ -0,0 +1,18 @@
+package com.serenitydojo.playwright.pageobjects;
+import com.microsoft.playwright.Page;
+public class NavBar {
+
+    private final Page page;
+
+    public NavBar(Page page) {
+        this.page = page;
+    }
+
+    public void openCart() {
+        page.getByTestId("nav-cart").click();
+    }
+
+    public void openHomePage() {
+        page.navigate("https://practicesoftwaretesting.com");
+    }
+}
